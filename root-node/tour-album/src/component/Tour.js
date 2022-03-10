005be0d627cbf026d637152994289b4466220543
dat-gif <70569870+dat-gif@users.noreply.github.com>
2022-03-10 20:54:55 +0700

    [code][Show more/less info tour]

--- root-node/tour-album/src/component/Tour.js
+++ root-node/tour-album/src/component/Tour.js
@@ -1,4 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -7,7 +10,12 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">{price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show less" : "Show more"}
+          </button>
+        </p>
         <button className="delete-btn">not interesting</button>
       </footer>
     </article>
